2430f8a8db4a95018032960f501ab1d4d766ef1a
Liliana <g.liliana@inbox.ru>
2021-12-12 12:27:31 +0300

    форма для ввода нового товара

--- view/create_invoice.py
+++ view/create_invoice.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 
 from src.organization import get_organization_list
 from view.create_organization import CreateOrganization
+from view.create_product import CreateProduct
 
 
 class CreateInvoice:
@@ -10,6 +11,7 @@ class CreateInvoice:
         self.create_main_menu = create_main_menu
         self.root = root
         self.content = content  # главное окно
+        self.product = Frame(self.content)
 
         self.orgs = None
         self.product_list = []
@@ -26,48 +28,52 @@ class CreateInvoice:
 
         Label(master=self.content, wraplength=1000, text="Создать счет-фактуру").grid(row=0, column=0, columnspan=5)
 
-        Button(self.content, text='Добавить организацию', command=self.add_organization).grid(row=1, column=0)
-        Button(self.content, text='Сохранить счет-фактуру', command=self.submit_invoice).grid(row=1, column=2)
+        Button(self.content, text='Добавить организацию', command=self.add_organization).grid(row=1, column=0, columnspan=4)
+        Button(self.content, text='Сохранить счет-фактуру', command=self.submit_invoice).grid(row=1, column=4, columnspan=2)
 
-        Label(master=self.content, wraplength=400, text="Поставщик").grid(row=2, column=0, columnspan=2)
-        self.provider = ttk.Combobox(master=self.content, values=orgs_name)
-        self.provider.grid(row=2, column=2, columnspan=3)
+        Label(master=self.content, text="Поставщик").grid(row=2, column=0)
+        self.provider = ttk.Combobox(master=self.content, values=orgs_name, width=60)
+        self.provider.grid(row=2, column=1, columnspan=2)
 
-        Label(master=self.content, wraplength=400, text="Покупатель").grid(row=3, column=0, columnspan=2)
-        self.buyer = ttk.Combobox(master=self.content, values=orgs_name)
-        self.buyer.grid(row=3, column=2, columnspan=3)
+        Label(master=self.content, text="Покупатель").grid(row=3, column=0)
+        self.buyer = ttk.Combobox(master=self.content, values=orgs_name, width=60)
+        self.buyer.grid(row=3, column=1, columnspan=2)
 
-        Label(master=self.content, wraplength=400, text="Грузоотправитель").grid(row=4, column=0, columnspan=2)
-        self.carrier = ttk.Combobox(master=self.content, values=orgs_name)
-        self.carrier.grid(row=4, column=2, columnspan=3)
+        Label(master=self.content, text="Грузоотправитель").grid(row=4, column=0)
+        self.carrier = ttk.Combobox(master=self.content, values=orgs_name, width=60)
+        self.carrier.grid(row=4, column=1, columnspan=2)
 
-        Label(master=self.content, wraplength=400, text="Грузополучатель").grid(row=5, column=0, columnspan=2)
-        self.consignee = ttk.Combobox(master=self.content, values=orgs_name)
-        self.consignee.grid(row=5, column=2, columnspan=3)
+        Label(master=self.content, text="Грузополучатель").grid(row=5, column=0)
+        self.consignee = ttk.Combobox(master=self.content, values=orgs_name, width=60)
+        self.consignee.grid(row=5, column=1, columnspan=2)
 
-        Label(master=self.content, wraplength=400, text="Дополнения").grid(row=6, column=0, columnspan=2)
+        Label(master=self.content, wraplength=400, text="Дополнения").grid(row=6, column=0)
         self.extensions = Entry(master=self.content, width=60)
-        self.extensions.grid(row=6, column=2, columnspan=3)
+        self.extensions.grid(row=6, column=1, columnspan=2)
 
-        self.submit_product()
+        self.add_product()
 
         self.content.pack()
 
     def add_product(self):
+        self.product.destroy()
+        self.product = Frame(self.content)
         # перерисовать список
-        Label(master=self.content, wraplength=200, text="Добавить товар").grid(row=7, column=0)
-        Label(master=self.content, wraplength=200, text="строка выбора товара").grid(row=7, column=1)
-        Label(master=self.content, wraplength=200, text="количество").grid(row=7, column=2)
-        self.count = Entry(master=self.content)
+        Label(master=self.product, text="Добавить товар").grid(row=7, column=0)
+        Label(master=self.product, text="строка выбора товара").grid(row=7, column=1)
+        Label(master=self.product, text="количество").grid(row=7, column=2)
+        self.count = Entry(master=self.product)
         self.count.grid(row=7, column=3)
-        Button(self.content, text='ОК', command=self.add_product).grid(row=7, column=4)
-        Button(self.content, text='Создать новый товар', command=self.submit_product).grid(row=7, column=5)
+        Button(self.product, text='ОК', command=self.add_product).grid(row=7, column=4)
+        Button(self.product, text='Создать новый товар', command=self.submit_product).grid(row=7, column=5)
 
         # product list
 
+        self.product.grid(row=7, column=0, columnspan=8)
+
     def submit_product(self):
-        # добавть новый
-        pass
+        prod = CreateProduct(self.root, self.content, self.add_invoice)
+        self.content = prod.content
 
     def submit_invoice(self):
         provider, buyer, carrier, consignee = None, None, None, None
---
+++ view/create_product.py
@@ -0,0 +1,102 @@
+from tkinter import *
+import re
+from tkinter import ttk
+
+from src.organization import create_organization
+
+
+class CreateProduct:
+    def __init__(self, root, content, add_invoice):
+        self.add_invoice = add_invoice
+        self.root = root
+        self.content = content  # главное окно
+
+        self.name_text = ''
+        self.measurement_text = ''
+        self.price_text = ''
+        self.excise_duty_text = ''
+        self.OKDP_text = ''
+        self.NDS_text = ''
+
+        self.add_product()
+
+    def add_product(self):
+        self.content.destroy()
+        self.content = Frame(self.root)
+
+        Label(master=self.content, wraplength=1000, text="Создать продукт").grid(row=0, column=0, columnspan=5)
+
+        Label(master=self.content, wraplength=400, text="Название").grid(row=1, column=0, columnspan=2)
+        self.name = Entry(master=self.content)
+        self.name.insert(0, self.name_text)
+        self.name.grid(row=1, column=2, columnspan=3)
+
+        Label(master=self.content, wraplength=400, text="Единица измерения").grid(row=2, column=0, columnspan=2)
+        self.measurement = Entry(master=self.content)
+        self.measurement.insert(0, self.measurement_text)
+        self.measurement.grid(row=2, column=2, columnspan=3)
+
+        Label(master=self.content, wraplength=400, text="Цена").grid(row=3, column=0, columnspan=2)
+        self.price = Entry(master=self.content)
+        self.price.insert(0, self.price_text)
+        self.price.grid(row=3, column=2, columnspan=3)
+
+        Label(master=self.content, wraplength=400, text="Сумма акциза для единицы товара").grid(row=4, column=0, columnspan=2)
+        self.excise_duty = Entry(master=self.content)
+        self.excise_duty.insert(0, self.excise_duty_text)
+        self.excise_duty.grid(row=4, column=2, columnspan=3)
+
+        Label(master=self.content, wraplength=400, text="НДС").grid(row=5, column=0, columnspan=2)
+        self.NDS = ttk.Combobox(master=self.content, values=['10%', '20%', 'товар, не облагаемый налогом', 'товар на экспорт'])
+        self.NDS.insert(0, self.NDS_text)
+        self.NDS.grid(row=5, column=2, columnspan=3)
+
+        Label(master=self.content, wraplength=400, text="Код ОКДП").grid(row=6, column=0, columnspan=2)
+        self.OKDP = Entry(master=self.content)
+        self.OKDP.insert(0, self.OKDP_text)
+        self.OKDP.grid(row=6, column=2, columnspan=3)
+
+        Button(self.content, text='Сохранить продукт', command=self.submit_product).grid(row=10)
+        self.content.pack()
+
+    def submit_product(self):
+        self.name_text = self.name.get().strip()
+        self.measurement_text = self.measurement.get().strip()
+        if re.fullmatch(r'\d*[.,]\d\d', self.price.get()):
+            self.price_text = self.price.get()
+        else:
+            self.price_text = ''
+
+        if re.fullmatch(r'\d*[.,]\d\d', self.excise_duty.get()):
+            self.excise_duty_text = self.excise_duty.get()
+        else:
+            self.excise_duty_text = ''
+        if re.fullmatch(r'\d{2,9}', self.OKDP.get()):
+            self.OKPO_text = int(self.OKDP.get())
+        else:
+            self.OKPO_text = ''
+
+        case_NDS = {
+            '10%': 1,
+            '20%': 2,
+            'товар, не облагаемый налогом': 32,
+            'товар на экспорт': 31
+        }
+        self.NDS_text = case_NDS[self.NDS.get()]
+
+        if not (self.name_text
+                and self.measurement_text
+                and self.price_text
+                and self.excise_duty_text
+                and self.OKPO_text
+                and self.NDS_text):
+            self.add_product()
+        else:
+            print(self.name_text
+                , self.measurement_text
+                , self.price_text
+                , self.excise_duty_text
+                , self.OKPO_text
+                , self.NDS_text)
+            self.content.destroy()
+            self.add_invoice()
