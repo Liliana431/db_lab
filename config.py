e996803695897dc0a3b3bc6a6ebf08094e3ff2f3
uBiC <albert.kogay@bk.ru>
2021-12-29 01:27:14 +0300

    test

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ connect = connect(dbname='sales_book', user='postgres',
                   password='1234qwer', host='localhost')
 CURSOR = connect.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 connect.autocommit = True
+# хуй
